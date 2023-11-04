a43f3fea935c1932d2c62efc0d2bd443a2fcbe78
Carlos-Mar14 <105378426+Carlos-Mar14@users.noreply.github.com>
2023-11-04 17:06:56 +0100

    Se crean componentes makeup, clothing, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { MakeupComponent } from './makeup/makeup.component';
+import { ClothingComponent } from './clothing/clothing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent,
+    MakeupComponent,
+    ClothingComponent
   ],
   imports: [
     BrowserModule,
