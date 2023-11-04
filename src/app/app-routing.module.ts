65ef92e7fbaf70317396ea0e792ee64c00e7e058
Carlos-Mar14 <105378426+Carlos-Mar14@users.noreply.github.com>
2023-11-04 20:28:26 +0100

    Se organizar rutas de menu y footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MakeupComponent } from './makeup/makeup.component';
+import { ClothingComponent } from './clothing/clothing.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'makeup', component: MakeupComponent },
+  { path: 'clothing', component: ClothingComponent },
+  { path: 'footer', component: FooterComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  constructor(private router: Router) {}
 
+  navigateToMakeup(): void {
+    this.router.navigate(['/makeup']);
+  }
+
+  navigateToClothing(): void {
+    this.router.navigate(['/clothing']);
+  }
+
+  navigateToFooter(): void {
+    this.router.navigate(['/footer']);
+  }
 }
